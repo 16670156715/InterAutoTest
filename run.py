394aaad8d888fe5a693cf2fec5a540075f282320
16670156715 <16670156715@163.com>
2020-03-28 23:16:33 +0800

    更新run.py

--- run.py
+++ run.py
@@ -7,4 +7,3 @@ if __name__ == '__main__':
     report_path = Conf.get_report_path() + os.sep + "result"
     report_html_path = Conf.get_report_path() + os.sep + "html"
     pytest.main(["-s", "test_excel_case.py", "--alluredir", report_path])
-
